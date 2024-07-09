002d244c146da575ea956699a65b551c8304970f
Sumit Nimbarte <smtnimbarte499@gmail.com>
2024-07-10 02:41:24 +0530

    implement debounce method

--- JavaScript/debounce.js
+++ JavaScript/debounce.js
@@ -1,1 +1,23 @@
 // debounce function
+
+function debounce(func, delay) {
+    let timer;
+    return function(){
+        let context = this;
+        let args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function() {
+                func.apply(context, args)
+        }, delay)
+    }
+}
+
+const getData = function(query){
+    console.log(query);
+}
+
+const delayGetData = debounce(getData, 1000);
+
+console.log(delayGetData("a"));
+console.log(delayGetData("ab"));
+console.log(delayGetData("abc"));
