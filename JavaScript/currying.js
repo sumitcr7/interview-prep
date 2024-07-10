b195aa30d70f0f6566dc2377a2e79e8b84ad9b78
Sumit Nimbarte <smtnimbarte499@gmail.com>
2024-07-11 02:39:36 +0530

    Update currying.js

--- JavaScript/currying.js
+++ JavaScript/currying.js
@@ -1,11 +1,15 @@
-function sum(a){
-    return function(b){
-        if(b !== undefined){
-            return sum(a+b);
-        }else{
-            return a;
-        }
-    }
-}
+// function sum(a){
+//     return function(b){
+//         if(b !== undefined){
+//             return sum(a+b);
+//         }else{
+//             return a;
+//         }
+//     }
+// }
+
+// es6 syntax
+
+const sum = a => b => b !== undefined ? sum(a+b) : a;
 
 console.log(sum(1)(2)(0)(4)())
