4eb07256a6faafd9c60076f55745e674cf1e3c8e
Sumit Nimbarte <smtnimbarte499@gmail.com>
2024-07-11 02:33:25 +0530

    Update currying.js

--- JavaScript/currying.js
+++ JavaScript/currying.js
@@ -1,11 +1,11 @@
 function sum(a){
     return function(b){
-        if(b === undefined){
-           return (a);
-        }else{
+        if(b !== undefined){
             return sum(a+b);
+        }else{
+            return a;
         }
     }
 }
 
-console.log(sum(1)(2)(3)(4)())
+console.log(sum(1)(2)(0)(4)())
